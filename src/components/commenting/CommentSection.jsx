d7bcc2e87eebd6a3ad6e19d1e4528482061443a0
radzarom <aadamrobson@gmail.com>
2023-02-07 16:45:47 +0000

    tried implementing no comments message

--- src/components/commenting/CommentSection.jsx
+++ src/components/commenting/CommentSection.jsx
@@ -5,12 +5,25 @@ import Comment from "./Comment.jsx";
 
 const CommentSection = ({ article_id }) => {
   const [comments, setComments] = useState([]);
+  const [noComments, setNoComments] = useState(true);
+
+  
 
   useEffect(() => {
     getCommentsForArticle(article_id).then((comments) => {
       setComments(comments);
+      if(comments.length != 0) setNoComments(false)
     });
   }, []);
+
+  if (noComments) {
+    return (
+      <section id="comment-section">
+        <h2>Comments</h2>
+        <p>There are no comments yet.</p>
+      </section>
+    );
+  }
 
   return (
     <section id="comment-section">
